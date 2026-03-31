ec0140bcd9652ae28430bbd5cba40a4ff1dfc5aa
Nikhil Yadav <nikhilyyadav2003@gmail.com>
2026-03-31 21:32:57 +0530

    added verify otp route

--- server.js
+++ server.js
@@ -605,7 +605,7 @@ app.get("/api/messages/unread/count", authMiddleware, async (req, res) => {
 
 
 const WebSocket = require("ws");
-
+const PORT = process.env.PORT || 8000;
 const server = app.listen(8000, () => {
   console.log("🔥 Server running on http://localhost:8000");
 });
