472e46660671b354f4e84fd786f889a7d714183f
Nikhil Yadav <nikhilyyadav2003@gmail.com>
2026-04-06 14:40:23 +0530

    added verify otp route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST", "PUT"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
